c308b4139e8694f5df1866e72fc5a5b76b63a1bd
Jchan <gkswocks123@gmail.com>
2025-02-23 00:47:23 +0900

    Chore : TokenAuthFilter 구현(헤더 토큰 검증)

--- src/main/java/kr/co/shop/makao/config/AuthConfig.java
+++ src/main/java/kr/co/shop/makao/config/AuthConfig.java
@@ -1,10 +1,12 @@
 package kr.co.shop.makao.config;
 
+import kr.co.shop.makao.filter.TokenAuthFilter;
 import kr.co.shop.makao.service.AuthTokenManager;
 import kr.co.shop.makao.service.AuthTokenManagerImpl;
 import kr.co.shop.makao.service.NoOpAuthTokenManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,15 @@ import java.util.Arrays;
 @Slf4j
 @Configuration
 public class AuthConfig {
+    @Bean
+    public FilterRegistrationBean<TokenAuthFilter> jwtAuthFilter(AuthTokenManager authTokenManager) {
+        FilterRegistrationBean<TokenAuthFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new TokenAuthFilter(authTokenManager));
+        registrationBean.addUrlPatterns("/not-working");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
     @Bean
     public AuthTokenManager authTokenManager(
             @Value("${auth.access-token.secret}") String accessTokenSecret,
