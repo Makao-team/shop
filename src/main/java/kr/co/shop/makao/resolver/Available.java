7c34a7a1398bd675070b3cdebacad035b2406a8d
Jchan <gkswocks123@gmail.com>
2025-03-04 17:31:11 +0900

    Refac : JwtPayload 제거(Available 어노테이션 대체 가능)

--- src/main/java/kr/co/shop/makao/resolver/Available.java
+++ src/main/java/kr/co/shop/makao/resolver/Available.java
@@ -1,4 +1,4 @@
-package kr.co.shop.makao.filter;
+package kr.co.shop.makao.resolver;
 
 import kr.co.shop.makao.enums.UserRole;
 
@@ -7,6 +7,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Available
+ * <p>
+ * Annotation for checking if the user has the required role to access the endpoint.
+ * The roles are defined in the {@link UserRole} enum.
+ * The annotation is used in the {@link kr.co.shop.makao.resolver.AuthHandlerMethodArgumentResolver} class.
+ * </p>
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Available {
