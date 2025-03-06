b852f1140a083c2234c025a1aaae0f7ab57ae26f
Jchan <gkswocks123@gmail.com>
2025-03-06 10:26:09 +0900

    Fix : JpaConfig 추가(EnableCommonJpa 어노테이션 동작 x)

--- src/main/java/kr/co/shop/makao/MakaoShopApplication.java
+++ src/main/java/kr/co/shop/makao/MakaoShopApplication.java
@@ -1,10 +1,8 @@
 package kr.co.shop.makao;
 
-import kr.co.shop.makao.config.EnableCommonJpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableCommonJpa
 @SpringBootApplication
 public class MakaoShopApplication {
     public static void main(String[] args) {
